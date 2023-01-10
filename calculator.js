404f263e7e785b5451ad05a0fc1971358f8b8ecf
Kyle Garner <garnerk10@gmail.com>
2023-01-10 12:59:25 -0500

    set up calculator display

--- calculator.js
+++ calculator.js
@@ -13,3 +13,8 @@ const multiply = (num1, num2) => {
 const divide = (num1, num2) => {
     return num1 / num2
 };
+
+const operate = (operator, num1, num2) => {
+    return operator(num1, num2);
+};
+
